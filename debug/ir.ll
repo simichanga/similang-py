3bbe972ae5651214acf5f47e988312e337f228f5
simichanga <siminischi1234@gmail.com>
2025-03-20 18:02:14 +0200

    feat: added benchmarking utilities inside main.py to identify bottlenecks. No clear bottlenecks in the pipeline stages: tokenization, parsing, compiling and executing found.

--- debug/ir.ll
+++ debug/ir.ll
@@ -11,22 +11,28 @@ define i32 @"main"()
 main_entry:
   %".2" = alloca i32
   store i32 0, i32* %".2"
+  %".4" = alloca i32
+  store i32 0, i32* %".4"
   br label %"for_loop_entry_1"
 for_loop_entry_1:
-  %".5" = load i32, i32* %".2"
-  %".6" = alloca [9 x i8]*
-  store [9 x i8]* @"__str_2", [9 x i8]** %".6"
-  %".8" = bitcast [9 x i8]* @"__str_2" to i8*
-  %".9" = call i32 (i8*, ...) @"printf"(i8* %".8", i32 %".5")
+  %".7" = load i32, i32* %".2"
+  %".8" = load i32, i32* %".4"
+  %".9" = add i32 %".7", %".8"
   %".10" = load i32, i32* %".2"
-  %".11" = add i32 %".10", 1
-  %".12" = load i32, i32* %".2"
-  store i32 %".11", i32* %".2"
-  %".14" = load i32, i32* %".2"
-  %".15" = icmp slt i32 %".14", 10
-  br i1 %".15", label %"for_loop_entry_1", label %"for_loop_otherwise_1"
+  store i32 %".9", i32* %".2"
+  %".12" = load i32, i32* %".4"
+  %".13" = add i32 %".12", 1
+  store i32 %".13", i32* %".4"
+  %".15" = load i32, i32* %".4"
+  %".16" = icmp slt i32 %".15", 100000
+  br i1 %".16", label %"for_loop_entry_1", label %"for_loop_otherwise_1"
 for_loop_otherwise_1:
+  %".18" = load i32, i32* %".2"
+  %".19" = alloca [11 x i8]*
+  store [11 x i8]* @"__str_2", [11 x i8]** %".19"
+  %".21" = bitcast [11 x i8]* @"__str_2" to i8*
+  %".22" = call i32 (i8*, ...) @"printf"(i8* %".21", i32 %".18")
   ret i32 0
 }
 
-@"__str_2" = internal constant [9 x i8] c"i = %i\0a\00\00"
+@"__str_2" = internal constant [11 x i8] c"Sum = %i\0a\00\00"
--- main.py
+++ main.py
@@ -10,7 +10,9 @@ from pipeline.compiler_debugger import debug_compiler
 from pipeline.executor import execute_code
 
 from utils.config import Config
+
 import logging
+import time
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG if Config.DEBUG else logging.INFO)
@@ -25,13 +27,23 @@ def load_code(file_path: str) -> str:
         logger.error(f"File not found: {file_path}")
         exit(1)
 
+# Configure benchmarking
+def benchmark_stage(stage_name, func, *args, **kwargs) -> any:
+    """Benchmark a specific stage of the compiler."""
+    start_time = time.time()
+    result = func(*args, **kwargs)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"{stage_name} took {elapsed_time:.4f} seconds")
+    return result
+
 def main():
     # Load code
     code = load_code('tests/test.simi')
 
     # Lexer
-    lexer = Lexer(source=code)
-    parser = Parser(lexer=lexer)
+    lexer: Lexer = benchmark_stage("Lexer", Lexer, source=code)
+    parser: Parser = benchmark_stage("Parser", Parser, lexer=lexer)
 
     if parser.errors:
         for error in parser.errors:
@@ -42,14 +54,14 @@ def main():
         debug_lexer(code)
 
     # Parse program
-    ast: Program = parser.parse_program()
+    ast: Program = benchmark_stage("Parsing", parser.parse_program)
 
     if Config.PARSER_DEBUG:
         debug_parser(parser, ast)
 
     # Compile
-    compiler: Compiler = Compiler()
-    compiler.compile(node=ast)
+    compiler: Compiler = benchmark_stage("Compiler", Compiler)
+    benchmark_stage("Compilation", compiler.compile, node=ast)
     module = compiler.module
 
     if Config.COMPILER_DEBUG:
@@ -57,7 +69,7 @@ def main():
 
     # Execute
     if Config.RUN_CODE:
-        execute_code(module)
+        benchmark_stage("Execution", execute_code, module)
 
 if __name__ == '__main__':
     main()
